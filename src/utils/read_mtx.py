93c9f54ae9b6129f4ce2ada4801bb7a68c3c1194
alex <alex.llobet@estudiantat.upc.edu>
2021-05-16 23:12:57 +0200

    mtx file reader

---
+++ src/utils/read_mtx.py
@@ -0,0 +1,25 @@
+from igraph import read
+from os import remove
+from networkx import Graph
+
+
+def read_mtx_file(filename:str) -> Graph:
+	# preprocess the file. Remove the two first lines of the header as they are for 
+	# explanatory purposes only (not part of the graph!!!). THIS ONLY WORKS WITH MTX FILES FROM
+	# networkrepository.com
+	with open(filename, 'r') as fin:
+    		data = fin.read().splitlines(True)
+	with open(f'{filename}_clean.mtx', 'w') as fout:
+    		fout.writelines(data[2:])
+    	
+    	# read the code in igraph, since this library can read clean mtx files.
+	g = read(f'{filename}_clean.mtx', format="edge")
+	
+	# clean all generated files (they were transitory)
+	remove(f'{filename}_clean.mtx') 
+	
+	# returns a networkx graph.
+	return g.to_networkx()
+	
+# uncomment for debugging purposes.
+#read_mtx_file("test.mtx")
